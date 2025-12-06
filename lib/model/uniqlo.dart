ff09f9ff3f81ebf021bdd3a87b47d3000a4c9f7f
Kanokpol508 <kanok508work@gmail.com>
2025-12-06 22:54:04 +0700

    Uniqlo List Design

---
+++ lib/model/uniqlo.dart
@@ -0,0 +1,69 @@
+class Uniqlo {
+  String imgUrl;
+  String imgTitle;
+
+  Uniqlo(this.imgUrl,this.imgTitle);
+
+  static List<Uniqlo> samples = [
+    Uniqlo(
+      'assets/images/uniqlo1.webp',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo2.png',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo3.webp',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo4.png',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo5.png',
+          'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo6.jpg',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo7.jpg',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo8.webp',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo9.webp',
+          'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo10.webp',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo11.png',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo12.webp',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo13.png',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo14.png',
+      'Basil Lemon Cooler',
+    ),
+    Uniqlo(
+      'assets/images/uniqlo15.png',
+      'Basil Lemon Cooler',
+    )
+  ];
+}
